bd760036fd4270cbf7e27ebf7b9287d1ade443cf
Nathan da Silva <111094286+Nathaandev@users.noreply.github.com>
2024-05-01 22:17:45 -0300

    Update Triangulos.java

--- Triangulos.java
+++ Triangulos.java
@@ -31,3 +31,19 @@ public class Main {
 
     }
 }
+
+
+
+Classe dos triangulos:
+
+package entities;
+
+public class Triangle {
+    public double a;
+    public double b;
+    public double c;
+    public double area() {
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
