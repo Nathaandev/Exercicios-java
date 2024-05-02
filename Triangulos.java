07c3fc10722981d085f1b09ad19322b43dda3efa
Nathan da Silva <111094286+Nathaandev@users.noreply.github.com>
2024-05-01 22:15:15 -0300

    Update and rename Triangulos to Triangulos.java
    Codigo que usa classes e funções para pedir 3 numeros de 2 triangulo, calcula as areas e devolve.

---
+++ Triangulos.java
@@ -0,0 +1,33 @@
+import entities.Triangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Triangle;
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+        System.out.println("Insire as medidas do triangulo X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Insire as medidas do triangulo Y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double areaX = x.area();
+        double areaY = y.area();
+        System.out.printf("Area do triangulo X: %f\n", areaX);
+        System.out.printf("Area do triangulo Y: %f\n", areaY);
+        if (areaX > areaY) {
+            System.out.printf("O triangulo com a maior area é o triangulo X com a area de: %f ", areaX);
+        } else {
+            System.out.printf("O triangulo com a maior area é o triangulo Y com a area de: %f ", areaY);
+        }
+
+    }
+}
