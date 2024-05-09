3773565ee61378686f276e117d943e64d8d35e2a
Nathan da Silva <111094286+Nathaandev@users.noreply.github.com>
2024-05-09 19:13:59 -0300

    Update Exercicio.java

--- Exercicio.java
+++ Exercicio.java
@@ -1,26 +1,21 @@
 package Exercicio;
-import java.util.Locale;
-import java.util.Scanner;
 
-public class Exercicio {
-    public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        Empregado empregado = new Empregado();
+public class Empregado {
+    public String nome;
+    public double SalarioBruto;
 
-        System.out.print("Digite o nome do empregado: ");
-        empregado.nome = sc.next();  // Pega o nome do usuário.
+    public double impostos;
 
-        System.out.print("Salário bruto: "); // Pega o salário do usuário.
-        empregado.SalarioBruto = sc.nextDouble();
-
-        System.out.print("Impostos: "); // Pega os impostos.
-        empregado.impostos = sc.nextDouble();
-
-        System.out.printf("Dados do empregado: %s", empregado); // Da o nome e o salário líquido.
-        System.out.printf("\nPorcentagem de aumento de salário: ", empregado.nome);
-        double porcentagem = sc.nextDouble(); // Recebe o aumento de salário.
-        empregado.AumentarSalario(porcentagem); // Aumenta o salário com base na porcentagem dada.
-        System.out.printf("Dados atualizados do empregado: %s", empregado); // Devolve os dados do usuário com nome e salário liquido após o aumento.
+    public double salariolimpo() {
+        return SalarioBruto - impostos;
     }
+
+    public void AumentarSalario(double porcetagem) {  // Função do aumento de salário.
+        SalarioBruto += SalarioBruto * porcetagem / 100;
+    }
+
+    public String toString(){
+         return ("Nome: " + nome + "\nSalário: R$" + String.format("%.2f", salariolimpo()));
+    }
+
 }
